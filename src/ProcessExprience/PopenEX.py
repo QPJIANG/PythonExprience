96118b5b4274d7773726a661388e249ce1f12af0
qpjiang <qiupingxyz@163.com>
2018-05-04 18:49:47 +0800

    popen cmd path

---
+++ src/ProcessExprience/PopenEX.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from subprocess import Popen, PIPE
+import os
+
+"""
+
+cmd in PATH is available
+alias is not available
+
+"""
+
+
+if __name__ == "__main__":
+
+    proc = Popen("which ll", stdout=PIPE, stderr=PIPE, shell=True, env=None, close_fds=False)
+    (stdout, stderr) = proc.communicate()
+    print stdout
+    os.system("lx")
+
+    pass
