07a17b167d01f9274ddf01d14454dc84b8900f61
qpjiang <qiupingxyz@163.com>
2018-05-09 22:50:23 +0800

    loggin

--- src/common/loggingx1/__init__.py
+++ src/common/loggingx1/__init__.py
@@ -1,9 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
+import logging.config
+import os
 
-"""
+from common.pathlibx import pathlibx
 
-if __name__ == "__main__":
-    pass
+
+def setup_logging():
+    conf = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'simple': {'format': '%(asctime)s | %(name)s | %(levelname)s | %(message)s'}
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'level': 'DEBUG',
+                'formatter': 'simple'
+            },
+            'info_file_handler': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'level': 'INFO',
+                'formatter': 'simple',
+                'filename': 'test.log',
+                'maxBytes': 1024,
+                'backupCount': 2,
+                'encoding': 'utf8'}
+        },
+        'loggers': {
+            'default': {
+                'level': 'DEBUG', 'handlers': ['console', 'info_file_handler'],
+                'propagate': False}
+        },
+        'roots': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': True}
+    }
+
+    logging_path = os.path.join(pathlibx.getRootPath(), "logs")
+    if not pathlibx.isExists(logging_path):
+        os.mkdir(logging_path)
+
+    logging_filename = os.path.join(logging_path, "default_log.log")
+    conf["handlers"]["info_file_handler"]["filename"] = logging_filename
+    logging.config.dictConfig(conf)
+
+
+setup_logging()
+
+logger = logging.getLogger("default")
--- src/common/loggingx2/__init__.py
+++ src/common/loggingx2/__init__.py
@@ -1,9 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
+import logging.config
+import os
+import yaml
+from logging.handlers import RotatingFileHandler
 
-"""
+from common.pathlibx import pathlibx
 
-if __name__ == "__main__":
-    pass
+
+class LogManager(object):
+    loggers = {}
+    config_ed = False
+    log_level = {
+        "CRITICAL": 50,
+        "FATAL": 50,
+        "ERROR": 40,
+        "WARNING": 30,
+        "WARN": 30,
+        "INFO": 20,
+        "DEBUG": 10,
+        "NOTSET": 0
+    }
+    def logging_conf(self):
+        config_file = os.path.join(pathlibx.getConfPath(), "logging.yaml")
+        with open(config_file, 'r', encoding='utf-8') as f:
+            cont = f.read()
+            x = yaml.load(cont)
+            return x
+
+    def _check_log_path(self):
+        logging_path = os.path.join(pathlibx.getRootPath(), "logs")
+        if not pathlibx.isExists(logging_path):
+            os.mkdir(logging_path)
+        pass
+
+    def getLogger(self, name):
+        _logger = LogManager.loggers.get(name, None)
+        if _logger is not None:
+            return _logger
+        else:
+            self._check_log_path()
+            logging_confs= self.logging_conf()
+
+            _logger = logging.getLogger(name)
+            formatter_str = '%(asctime)s : %(name)s : %(levelname)s: %(message)s'
+            formatter = logging.Formatter(formatter_str)
+            level = LogManager.log_level.get(logging_confs.get("logging_level"))
+
+            logging.basicConfig(level=level, format=formatter_str)
+            logging_path = os.path.join(pathlibx.getRootPath(), "logs")
+            logging_filename = os.path.join(logging_path, name + ".log")
+
+            r_handler = RotatingFileHandler(logging_filename, maxBytes=30 * 1024, backupCount=3)
+            r_handler.setFormatter(formatter)
+
+            console = logging.StreamHandler()
+            console.setFormatter(formatter)
+
+            _logger.addHandler(r_handler)
+            _logger.addHandler(console)
+
+            LogManager.loggers[name] = _logger
+            return _logger
+
+
+logging_manager = LogManager()
